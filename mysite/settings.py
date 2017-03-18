b98d39f7244cb2fd8c04c7060cd773cec8b245f4
jacobsongs <jacobj1991@gmail.com>
2017-03-18 12:48:58 +0530

    setting logging

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,6 +25,47 @@ SECRET_KEY = '6tg#)*(i_6ip7hnv=+tmr9#ygc*fo+a%pg_4)%#02!t!vi$@!n'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'filters': {
+        'require_debug_false': {
+            '()': 'django.utils.log.RequireDebugFalse'
+        }
+    },
+    'handlers': {
+        'mail_admins': {
+            'level': 'ERROR',
+            'filters': ['require_debug_false'],
+            'class': 'django.utils.log.AdminEmailHandler'
+        },
+        'applogfile': {
+            'level':'DEBUG',
+            'class':'logging.handlers.RotatingFileHandler',
+            'filename': os.path.join(DJANGO_ROOT, 'APPNAME.log'),
+            'maxBytes': 1024*1024*15, # 15MB
+            'backupCount': 10,
+        },
+    },
+    'loggers': {
+        'django.request': {
+            'handlers': ['mail_admins'],
+            'level': 'ERROR',
+            'propagate': True,
+        },
+        'django.request': {
+            'handlers': ['mail_admins'],
+            'level': 'ERROR',
+            'propagate': True,
+        },
+        'APPNAME': {
+            'handlers': ['applogfile',],
+            'level': 'DEBUG',
+        },
+    }
+}
+
+
 ALLOWED_HOSTS = ['jacobsongs.herokuapp.com']
 
 
