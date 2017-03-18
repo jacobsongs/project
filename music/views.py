8f76273a61c2de93e5301ad88f5dfdf3951de397
jacobsongs <jacobj1991@gmail.com>
2017-03-18 11:26:19 +0530

    views

--- music/views.py
+++ music/views.py
@@ -3,6 +3,20 @@ from django.views.generic.edit import CreateView,UpdateView,DeleteView
 from django.core.urlresolvers import reverse_lazy
 from .models import Album
 
+def get_ip_address(request):
+    """ use requestobject to fetch client machine's IP Address """
+    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+    if x_forwarded_for:
+        ip = x_forwarded_for.split(',')[0]
+    else:
+        ip = request.META.get('REMOTE_ADDR')    ### Real IP address of client Machine
+    return ip   
+
+
+def home(request):
+    """ your vies to handle http request """
+    ip_address = get_ip_address(request)
+    
 class IndexView(generic.ListView):
 	template_name="Music/index.html"
 	context_object_name='all_albums'
