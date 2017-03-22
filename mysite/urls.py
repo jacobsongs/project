2128baa4a1d6d0dae6b97d10334341054ce7b3fa
jacobsongs <jacobj1991@gmail.com>
2017-03-22 17:28:21 +0530

    urls

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,10 +1,7 @@
 from django.conf.urls import include, url
-from django.http import HttpResponse
 from django.contrib import admin
-
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^music/', include('music.urls', namespace="music")),
-    (r'^robots.txt$', lambda r: HttpResponse("User-agent: *\nDisallow: /", content_type="text/plain")),
+    url(r'^music/', include('music.urls', namespace="music"))
 ]
