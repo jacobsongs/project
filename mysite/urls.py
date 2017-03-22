d9a2f409cbe778b445a38fa989bc5411764453b3
jacobsongs <jacobj1991@gmail.com>
2017-03-22 16:54:02 +0530

    urls

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import include, url
-from django.contrib import admin
 from django.http import HttpResponse
+from django.contrib import admin
+
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
