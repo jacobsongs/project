71523e6d6d82b90839eab0109449c3ad6c9fecc9
jacobsongs <jacobj1991@gmail.com>
2017-03-23 10:45:59 +0530

    forms

--- music/urls.py
+++ music/urls.py
@@ -7,6 +7,8 @@ urlpatterns = [
     url(r'^$', views.IndexView.as_view(),name="index"),
     url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
     url(r'^album/add$', views.AlbumCreate.as_view(), name='album-add'),
-
+    url(r'^$', views.index, name='home'),
+    # new url definition
+    url(r'^contact/$', views.contact, name='contact'),
    
 ]
