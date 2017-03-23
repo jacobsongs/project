b25b772eb36e9b3e6971e79482716ff8ce67ebcc
jacobsongs <jacobj1991@gmail.com>
2017-03-23 11:48:31 +0530

    about

--- mysite/urls.py
+++ mysite/urls.py
@@ -5,7 +5,7 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^music/', include('music.urls', namespace="music")),
-    url(r'aboutus/',include('aboutus.urls', namespace="aboutus"))
+    url(r'^aboutus/',include('aboutus.urls', namespace="aboutus")),
     url(r'^robots.txt$', TemplateView.as_view(template_name="robots.txt", content_type="text/plain"), name="robots_file")
 
 ]
