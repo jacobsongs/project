903d80a1a062fafe8ed201c0886da689e2a77e95
jacobsongs <jacobj1991@gmail.com>
2017-03-23 10:15:00 +0530

    url

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import include, url
-from django.http import HttpResponse
+from django.views.generic import TemplateView
 from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^music/', include('music.urls', namespace="music")),
+    url(r'^robots.txt$', TemplateView.as_view(template_name="robots.txt", content_type="text/plain"), name="robots_file")
+
 ]
