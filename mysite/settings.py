a57652daffa199f7149b0abfd90b290ea731e46d
jacobsongs <jacobj1991@gmail.com>
2017-03-29 12:27:18 +0530

    settings

--- mysite/settings.py
+++ mysite/settings.py
@@ -83,7 +83,15 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'django.middleware.security.SecurityMiddleware',
-)
+    
+from django.conf import settings
+from django import http
+class BlockedIpMiddleware(object):
+
+    def process_request(self, request):
+        if request.META['REMOTE_ADDR'] in settings.BLOCKED_IPS:
+            return http.HttpResponseForbidden('<h1>Forbidden</h1>')
+        return None)
 
 ROOT_URLCONF = 'mysite.urls'
 
